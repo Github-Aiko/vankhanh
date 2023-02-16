b3cb17197d30f670dbe28163acde019a3d9c43f0
CLOUDFLY <124904465+Vankhanh1996@users.noreply.github.com>
2023-02-16 15:47:28 +0700

    80XrayR

--- 80443yuna.sh
+++ 80443yuna.sh
@@ -1,101 +1,107 @@
-#!/bin/bash
-# bản chạy 443 và 80 xrayr thường
-bash <(curl -ls https://raw.githubusercontent.com/AZZ-vopp/Z_OV/main/xrayrthuong.sh)
+#!/binstall
 
+# 控制台字体
+red() {
+	echo -e "\033[31m\033[01m$1\033[0m"
+}
 
-    read -p "Nhập Link Web :" domain
-    echo -e "Link Web là : ${domain}"
+green() {
+	echo -e "\033[32m\033[01m$1\033[0m"
+}
 
-    read -p "Nhập Api Key :" APIKEY
-    echo -e "API KEY là : ${APIKEY}"
+yellow() {
+	echo -e "\033[33m\033[01m$1\033[0m"
+}
 
-    read -p "Nhập Node ID port 80 :" node_80
-    echo -e "Node_80 là : ${node_80}"
+# 判断系统及定义系统安装依赖方式
+REGEX=("debian" "ubuntu" "centos|red hat|kernel|oracle linux|alma|rocky" "'amazon linux'")
+RELEASE=("Debian" "Ubuntu" "CentOS" "CentOS" "Alpine")
+PACKAGE_UPDATE=("apt -y update" "apt -y update" "yum -y update" "yum -y update")
+PACKAGE_INSTALL=("apt -y install" "apt -y install" "yum -y install" "yum -y install" "apk add -f")
+PACKAGE_REMOVE=("apt -y remove" "apt -y remove" "yum -y remove" "yum -y remove")
 
-    read -p "Nhập Node ID port 443 :" node_443
-    echo -e "Node_443 là : ${node_443}"    
+CMD=("$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)" "$(hostnamectl 2>/dev/null | grep -i system | cut -d : -f2)" "$(lsb_release -sd 2>/dev/null)" "$(grep -i description /etc/lsb-release 2>/dev/null | cut -d \" -f2)" "$(grep . /etc/redhat-release 2>/dev/null)" "$(grep . /etc/issue 2>/dev/null | cut -d \\ -f1 | sed '/^[ ]*$/d')")
 
-    read -p "Nhập SpeedLimit :" SpeedLimit
-    echo -e "SpeedLimit = ${SpeedLimit}"
+for i in "${CMD[@]}"; do
+	SYS="$i" && [[ -n $SYS ]] && break
+done
 
-    read -p "Nhập DeviceLimit :" DeviceLimit
-    echo -e "DeviceLimit = ${DeviceLimit}"
+for ((int = 0; int < ${#REGEX[@]}; int++)); do
+	[[ $(echo "$SYS" | tr '[:upper:]' '[:lower:]') =~ ${REGEX[int]} ]] && SYSTEM="${RELEASE[int]}" && [[ -n $SYSTEM ]] && break
+done
 
-    read -p "Nhập dns trên cloudflare node 443:" CertDomain443
-    echo -e "CertDomain443 = ${CertDomain443}"           
-  
-cd /etc/XrayR
+[[ -z $SYSTEM ]] && red "不支持VPS的当前系统，请使用主流的操作系统" && exit 1
 
-EOF
-cat >config.yml <<EOF
+archAffix() {
+	case "$(uname -m)" in
+  x86_64 | x64 | amd64) return 0 ;;
+	aarch64 | arm64) return 0 ;;
+	*) red "不支持的CPU架构，脚本即将退出！" ;;
+	esac
+
+	return 0
+}
+
+install() {
+	install_XrayR
+	clear
+	makeConfig
+}
+
+install_XrayR() {
+	[[ -z $(type -P curl) ]] && ${PACKAGE_UPDATE[int]} && ${PACKAGE_INSTALL[int]} curl
+	[[ -z $(type -P socat) ]] && ${PACKAGE_UPDATE[int]} && ${PACKAGE_INSTALL[int]} socat
+	bash <(curl -Ls https://raw.githubusercontent.com/XrayR-project/XrayR-release/master/install.sh)  
+}
+
+makeConfig() {
+	echo "---------------"
+	read -p "Nhập link website ( https://2lands.me ) :" airWebsite
+	echo "---------------"
+	read -p "API key của web :" airAPIKey
+	echo "---------------"
+	read -p "Số node ID :" makeNodeID
+	echo "---------------"
+	read -p "Giới hạn số thiết bị, nếu không muốn giới hạn hãy nhập 0 :" makeLimitdevice
+	echo "---------------"
+	read -p "Giới hạn tốc độ, nếu không muốn giới hạn nhập 0 :" makeSpeedlimit
+	echo "---------------"
+
+	rm -f /etc/XrayR/config.yml
+	if [[ -z $(~/.acme.sh/acme.sh -v 2>/dev/null) ]]; then
+		curl https://get.acme.sh | sh -s email=script@github.com
+		source ~/.bashrc
+		bash ~/.acme.sh/acme.sh --upgrade --auto-upgrade
+	fi
+         cat <<EOF >/etc/XrayR/config.yml
 Log:
   Level: none # Log level: none, error, warning, info, debug 
   AccessPath: # /etc/XrayR/access.Log
   ErrorPath: # /etc/XrayR/error.log
-DnsConfigPath: # /etc/XrayR/dns.json Path to dns config, check https://xtls.github.io/config/base/dns/ for help
-RouteConfigPath: # /etc/XrayR/route.json # Path to route config, check https://xtls.github.io/config/base/route/ for help
-OutboundConfigPath: # /etc/XrayR/custom_outbound.json # Path to custom outbound config, check https://xtls.github.io/config/base/outbound/ for help
+DnsConfigPath: # /etc/XrayR/dns.json # Path to dns config, check https://xtls.github.io/config/dns.html for help
+RouteConfigPath: # /etc/XrayR/route.json # Path to route config, check https://xtls.github.io/config/routing.html for help
+InboundConfigPath: # /etc/XrayR/custom_inbound.json # Path to custom inbound config, check https://xtls.github.io/config/inbound.html for help
+OutboundConfigPath: # /etc/XrayR/custom_outbound.json # Path to custom outbound config, check https://xtls.github.io/config/outbound.html for help
 ConnetionConfig:
-  Handshake: 4 # Handshake time limit, Second
-  ConnIdle: 10 # Connection idle time limit, Second
-  UplinkOnly: 2 # Time limit when the connection downstream is closed, Second
-  DownlinkOnly: 4 # Time limit when the connection is closed after the uplink is closed, Second
-  BufferSize: 64 # The internal cache size of each connection, kB 
+  Handshake: 2 # Handshake time limit, Second
+  ConnIdle: 200 # Connection idle time limit, Second
+  UplinkOnly: 0 # Time limit when the connection downstream is closed, Second
+  DownlinkOnly: 0 # Time limit when the connection is closed after the uplink is closed, Second
+  BufferSize: 1 # The internal cache size of each connection, kB 
 Nodes:
   -
     PanelType: "V2board" # Panel type: SSpanel, V2board, PMpanel, Proxypanel
     ApiConfig:
-      ApiHost: "$domain"
-      ApiKey: "$APIKEY"
-      NodeID: $node_80
+      ApiHost: "$airWebsite"
+      ApiKey: "$airAPIKey"
+      NodeID: $makeNodeID
       NodeType: V2ray # Node type: V2ray, Trojan, Shadowsocks, Shadowsocks-Plugin
-      Timeout: 30 # Timeout for the api request
+      Timeout: 10 # Timeout for the api request
       EnableVless: false # Enable Vless for V2ray Type
       EnableXTLS: false # Enable XTLS for V2ray and Trojan
-      SpeedLimit: $SpeedLimit # Mbps, Local settings will replace remote settings, 0 means disable
-      DeviceLimit: $DeviceLimit # Local settings will replace remote settings, 0 means disable
-      RuleListPath: /etc/XrayR/AikoBlock # /etc/XrayR/rulelist Path to local rulelist file
-    ControllerConfig:
-      ListenIP: 0.0.0.0 # IP address you want to listen
-      SendIP: 0.0.0.0 # IP address you want to send pacakage
-      UpdatePeriodic: 60 # Time to update the nodeinfo, how many sec.
-      EnableDNS: false # Use custom DNS config, Please ensure that you set the dns.json well
-      DNSType: AsIs # AsIs, UseIP, UseIPv4, UseIPv6, DNS strategy
-      DisableUploadTraffic: false # Disable Upload Traffic to the panel
-      DisableGetRule: false # Disable Get Rule from the panel
-      DisableIVCheck: false # Disable the anti-reply protection for Shadowsocks
-      DisableSniffing: true # Disable domain sniffing 
-      EnableProxyProtocol: false # Only works for WebSocket and TCP
-      EnableFallback: false # Only support for Trojan and Vless
-      FallBackConfigs:  # Support multiple fallbacks
-        -
-          SNI: # TLS SNI(Server Name Indication), Empty for any
-          Path: # HTTP PATH, Empty for any
-          Dest: 80 # Required, Destination of fallback, check https://xtls.github.io/config/fallback/ for details.
-          ProxyProtocolVer: 0 # Send PROXY protocol version, 0 for dsable
-      CertConfig:
-        CertMode: none # Option about how to get certificate: none, file, http, dns. Choose "none" will forcedly disable the tls config.
-        CertDomain: "node1.test.com" # Domain to cert
-        CertFile: /etc/XrayR/cert/node1.test.com.cert # Provided if the CertMode is file
-        KeyFile: /etc/XrayR/cert/node1.test.com.key
-        Provider: cloudflare # DNS cert provider, Get the full support list here: https://go-acme.github.io/lego/dns/
-        Email: test@me.com
-        DNSEnv: # DNS ENV option used by DNS provider
-          CLOUDFLARE_EMAIL: hatoo.senpai20@gmail.com
-          CLOUDFLARE_API_KEY: 8953ed5c62da1e2539ae1bfc2e2e60f0d9d99
-  -
-    PanelType: "V2board" # Panel type: SSpanel, V2board, PMpanel, Proxypanel
-    ApiConfig:
-      ApiHost: "$domain"
-      ApiKey: "$APIKEY"
-      NodeID: $node_443
-      NodeType: V2ray # Node type: V2ray, Trojan, Shadowsocks, Shadowsocks-Plugin
-      Timeout: 30 # Timeout for the api request
-      EnableVless: false # Enable Vless for V2ray Type
-      EnableXTLS: false # Enable XTLS for V2ray and Trojan
-      SpeedLimit: $SpeedLimit # Mbps, Local settings will replace remote settings, 0 means disable
-      DeviceLimit: $DeviceLimit # Local settings will replace remote settings, 0 means disable
-      RuleListPath: /etc/XrayR/AikoBlock # /etc/XrayR/rulelist Path to local rulelist file
+      SpeedLimit: $makeSpeedlimit # Mbps, Local settings will replace remote settings, 0 means disable
+      DeviceLimit: $makeLimitdevice # Local settings will replace remote settings, 0 means disable
+      RuleListPath: # /etc/XrayR/rulelist Path to local rulelist file
     ControllerConfig:
       ListenIP: 0.0.0.0 # IP address you want to listen
       SendIP: 0.0.0.0 # IP address you want to send pacakage
@@ -116,14 +122,18 @@ Nodes:
           ProxyProtocolVer: 0 # Send PROXY protocol version, 0 for dsable
       CertConfig:
         CertMode: dns # Option about how to get certificate: none, file, http, dns. Choose "none" will forcedly disable the tls config.
-        CertDomain: "$CertDomain443" # Domain to cert
-        CertFile: /etc/XrayR/cert/node1.test.com.cert # Provided if the CertMode is file
+        CertDomain: "node1.test.com" # Domain to cert
+        CertFile: /etc/XrayR/cert/node1.test.com.cert  # Provided if the CertMode is file
         KeyFile: /etc/XrayR/cert/node1.test.com.key
-        Provider: cloudflare # DNS cert provider, Get the full support list here: https://go-acme.github.io/lego/dns/
+        Provider: alidns # DNS cert provider, Get the full support list here: https://go-acme.github.io/lego/dns/
         Email: test@me.com
         DNSEnv: # DNS ENV option used by DNS provider
-          CLOUDFLARE_EMAIL: hatoo.senpai20@gmail.com
-          CLOUDFLARE_API_KEY: 8953ed5c62da1e2539ae1bfc2e2e60f0d9d99
+          ALICLOUD_ACCESS_KEY: khanhdubai1996@gmail.com
+          ALICLOUD_SECRET_KEY: d707a0b5e766d7675d8b416fdb28ac6b316ad
 EOF
-cd /root
-xrayr restart
+	xrayr restart
+	green "Đã cài đặt và cập nhật XrayR với bảng điều khiển thành công！"
+	exit 1
+}
+
+install
